ed96007d083dbf4f808e775317d3aed53d1a0580
Christopher Stolt <stolt46@gmail.com>
2022-12-07 13:49:16 -0700

    adding DOMAIN_NAME subs to apply-manifest

--- apply-manifest.sh
+++ apply-manifest.sh
@@ -4,4 +4,5 @@ cat manifest.yaml |
   sed -e "s|\${ACME_HMAC_KEY}|${ACME_HMAC_KEY}|g" |
   sed -e "s|\${ACME_DIRECTORY_URL}|${ACME_DIRECTORY_URL}|g" |
   sed -e "s|\${ACME_KID}|${ACME_KID}|g" |
+  sed -e "s|\${DOMAIN_NAME}|${DOMAIN_NAME}|g" |
   kubectl apply -f -
